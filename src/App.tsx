f6055ceac84a4310947f3fd3ac3d75d9fd28b7f1
Toto <19580671+tomso11@users.noreply.github.com>
2022-04-21 13:41:35 +0200

    add header font lol

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-
+      <header className="App-header header">
+      <h1>Clientversity</h1>
       </header>
       <Plot data={[
         {
