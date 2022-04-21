0855dcf5a86fe171aa37a831b5d7b367f0cf0995
Agustin Vazquez <agustin@poap.io>
2022-04-21 13:25:29 +0200

    Api

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type ClientAffinity = {
+    "Uncertain": number;
+    "Lighthouse": number;
+    "Lodestar": number;
+    "Nimbus": number;
+    "Other": number;
+    "Prysm": number; 
+    "Teku": number;
+};
